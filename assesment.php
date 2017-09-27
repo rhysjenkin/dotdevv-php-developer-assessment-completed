53adf1fad51959eda8534c2df138e71e9ce6e7e4
Alex <alex@dotdev.com.au>
2017-09-27 12:26:40 +1000

    :tada: Initial Commit

---
+++ assesment.php
@@ -0,0 +1,80 @@
+<?php
+/*
+* DotDev - PHP Developer Test
+* Author:
+* Date Completed:
+* Time taken: 0h 0m
+* Remarks:
+*   - Modules
+*   - Errors
+*/
+
+class StoreData {
+    function __construct() {
+    }
+
+    public function loadData () {
+        $customers = (object) [
+            ['id' => 'BQYLCQ0CCwIOBgYNBAcACw', 'name' => 'Bob'],
+            ['id' => 'CQwPDAkDDAQLBQsOBAcMBw', 'name' => 'Jan'],
+            ['id' => 'AgsIBAsFAwYCCw8GBAINAQ', 'name' => 'Steve'],
+            ['id' => 'DAEFDQwPDwMCCwULBAAMDg', 'name' => 'Fred'],
+            ['id' => 'DQkCAAYHAAMJBA4LBAUOCg', 'name' => 'Robot']
+        ];
+        $orders = (object) [
+            ['id' => 'DwsNDQ4JDQEEBQIJBAwNBA', 'customerId' => 'BQYLCQ0CCwIOBgYNBAcACw', 'dateOrdered' => 1506476504],
+            ['id' => 'DwsPBQ0BAA0BBwwMBAoECA', 'customerId' => 'BQYLCQ0CCwIOBgYNBAcACw', 'dateOrdered' => 1506480104],
+            ['id' => 'DAEFCwUAAgQPAQIIBA4IBA', 'customerId' => 'CQwPDAkDDAQLBQsOBAcMBw', 'dateOrdered' => 1506562904],
+            ['id' => 'BAUNCAUAAQYMDgULBAMDAQ', 'customerId' => 'CgUDCQ8IDAsIBwUBBAgIAA', 'dateOrdered' => 1507081304],
+            ['id' => 'DAMGAg8GCggLBwkJBAoECg', 'customerId' => 'AgsIBAsFAwYCCw8GBAINAQ', 'dateOrdered' => 1509068504],
+            ['id' => 'CQALBwoDAw0AAQgHBAEJBQ', 'customerId' => 'DAEFDQwPDwMCCwULBAAMDg', 'dateOrdered' => 1538012504]
+        ];
+        $order_items = (object) [
+            ['id' => 'DwsNDQ4JDQEEBQIJBAwNBA', 'items' => [
+                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 10.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
+                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
+                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                ]
+            ],
+            ['id' => 'DwsPBQ0BAA0BBwwMBAoECA', 'items' => [
+                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 90.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
+                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
+                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                ]
+            ],
+            ['id' => 'DAEFCwUAAgQPAQIIBA4IBA', 'items' => [
+                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 3.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
+                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,  'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
+                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 15.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                ]
+            ],
+            ['id' => 'BAUNCAUAAQYMDgULBAMDAQ', 'items' => [
+                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 10.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
+                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
+                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                ]
+            ],
+            ['id' => 'DAMGAg8GCggLBwkJBAoECg', 'items' => [
+                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 32.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
+                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
+                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                ]
+            ]
+        ];
+    }
+
+    public function formatData ($option) {
+        // All data should be returned as formatted JSON.
+        if ($option = 1) {
+            // return orders sorted by highest value. Be sure to include the order total in the response
+        } elseif ($option = 2) {
+            // return orders sorted by date
+        } elseif ($option = 3) {
+            // return orders without items
+        }
+        print 'DotDev';
+    }
+}
+
+$run = new StoreData();
+$run->formatData($option);
---
+++ readme.md
@@ -0,0 +1,55 @@
+# ![DotDev](https://avatars3.githubusercontent.com/u/19314960?v=3&s=200)
+
+## DotDev PHP Developer Assesment
+
+The goal of assesment is to test you skills in data structures and formatting.
+
+We have created three data objects `customers`, `orders` and `order_items` in `function loadData()`.
+
+The application should take and integer from stdin or a query string parameter in a web browser and return the approriately formatted data type using data from all three objects.
+
+**Your task is to:**
+
+- Update the function `formatData` to complete the data formatting task.
+- Update `$run` to determine if the program is run via the CLI or browser
+- You can either create a git repo and send us the link or provide a copy of your git log.
+
+- Do **NOT** use a framework. We want to see your handle of PHP, not how well you can use a framework.
+- If you use any modules from composer document your reasoning why you chose the package in the `remarks` section of the code.
+- ***The data may not be formatted correctly** Document any errors in the `remarks` section
+- The application only needs to run on PHP 7 or higher.
+
+### Usage:
+
+#### 🖥️ CLI:
+
+`~/php assesment.php $option`
+
+#### 🌏 Browser:
+
+` php -S localhost:8000 - assesment.php`
+
+
+### Example expected results
+
+```json
+
+{
+    "orders": {
+        "DwsNDQ4JDQEEBQIJBAwNBA" : {
+            "date": 1506476504,
+            "total": 101.00,
+            "customer" : {
+                "id": "BQYLCQ0CCwIOBgYNBAcACw",
+                "name": "Bob"
+            },
+            "order_items": [{
+                "id": "CgkCDwwDDgYODgYFBAwKAQ",
+                "name": "cf3298bb5cbfd41aa44ba18b4f305a36",
+                "value": 101.00
+            }]
+        }
+    }
+}
+
+```
